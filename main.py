cd50371563bed8a372726808f00a90f7ad1171f8
hallma886 <hallma886@northwested.org>
2024-11-06 13:32:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,116 @@
+# Matthew Hall
+# 28 November 2024
+# for-loops-with-lists
+
+# Task 4-1: Pizzas
+pizzas = ["Cheese Pizza","Pepperoni Pizza", "Deep Dish Pizza"]
+for p in pizzas:
+    print(f'I like {p}')
+print()
+# Task 4-2: Animals
+animals = ['Apes', 'Gorillas', 'Orangutans']
+for a in animals:
+    print(f'{a} are in the primate family')
+print()
+print('All of these animals are in the primate family. (along with humans who are also in the primate family)')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
